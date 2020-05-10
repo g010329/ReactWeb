3e40c4be6d17694ecdbf4969463d605f55362629
Grace <g010329@gmail.com>
2020-05-10 18:52:23 +0800

    Updates

---
+++ precache-manifest.f475d0c7e8601bf3e678008ef1b3762b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ed2845f09c040e5079198ab50ae1d3",
+    "url": "/ReactWeb/index.html"
+  },
+  {
+    "revision": "dfdc057773cac893c3dc",
+    "url": "/ReactWeb/static/css/main.fe7f79cb.chunk.css"
+  },
+  {
+    "revision": "f6c6c0b9800388d0e228",
+    "url": "/ReactWeb/static/js/2.dfdad824.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactWeb/static/js/2.dfdad824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfdc057773cac893c3dc",
+    "url": "/ReactWeb/static/js/main.d85d7be2.chunk.js"
+  },
+  {
+    "revision": "755b282a75dcc4d48272",
+    "url": "/ReactWeb/static/js/runtime-main.de53d5cd.js"
+  },
+  {
+    "revision": "c176c44cff304736cf9422b2545d1bd6",
+    "url": "/ReactWeb/static/media/iconfinder_Star_5349757.c176c44c.png"
+  }
+]);
